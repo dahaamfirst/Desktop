c4755f4b577fbfeed24e822404308ef7a246f536
dahaamfirst <132234794+dahaamfirst@users.noreply.github.com>
2025-06-22 02:03:06 +0300

    update

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-import psycopg2
-from flask_sqlalchemy import SQLAlchemy
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
-db = SQLAlchemy(app)
